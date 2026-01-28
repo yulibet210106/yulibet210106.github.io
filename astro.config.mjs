fd50a535cb2a8bf06f92f66d213194fb81b9ea4c
Carolina Mendoza Quiroga <yulibetquiroga@gmail.com>
2026-01-28 16:22:37 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://yulibet210106.github.io",
+
+  integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react()],
 });
